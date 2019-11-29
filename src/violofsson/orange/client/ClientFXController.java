7be1997a842b7110e02a493a4ea5f0e0f2805475
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2019-11-29 22:23:01 +0100

    Mer JavaFX

--- src/violofsson/orange/client/ClientFXController.java
+++ src/violofsson/orange/client/ClientFXController.java
@@ -2,14 +2,21 @@ package violofsson.orange.client;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import violofsson.orange.protocol.Question;
+import violofsson.orange.protocol.ServerMessage;
 
-public class ClientFXController {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientFXController extends Thread {
     @FXML
     HBox categoryPanel;
 
@@ -36,12 +43,113 @@ public class ClientFXController {
     @FXML
     void chooseAnswer(ActionEvent actionEvent) {
         String answer = ((Button) actionEvent.getSource()).getText();
+        setAnswersDisable(true);
         session.write(answer);
     }
 
     @FXML
     void chooseCategory() {
         String chosen = categoryChooser.getSelectionModel().getSelectedItem();
+        setCategoryDisable(true);
         session.write(chosen);
+    }
+
+    @Override
+    public void run() {
+        Object obj;
+
+        try {
+            while ((obj = session.read()) != null) {
+                if (obj instanceof Question) {
+                    Question question = (Question) obj;
+                    displayQuestion(question);
+                } else if (obj instanceof ServerMessage) {
+                    ServerMessage fromServer = (ServerMessage) obj;
+                    processServerMessage(fromServer);
+                } else if (obj instanceof String) {
+                    String message = (String) obj;
+                    displayMessage(message);
+                } else if (obj instanceof Integer[]) {
+                    Integer[] points = (Integer[]) obj;
+                    //showPoints(points);
+                } else if (obj instanceof ArrayList) {
+                    // Kontrollera typer!
+                    ArrayList<List> lista = (ArrayList) obj;
+                    List<Integer> playerOneHistory = lista.get(0);
+                    List<Integer> playerTwoHistory = lista.get(1);
+                    /*String playerOneText = getScoreSummary("Spelare 1",
+                            playerOneHistory);
+                    String playerTwoText = getScoreSummary("Spelare 2",
+                            playerTwoHistory);
+                    JOptionPane.showMessageDialog(this, playerOneText + "\n\n" + playerTwoText);*/
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void displayMessage(String msg) {
+        messagePanel.setText(msg);
+    }
+
+    void displayQuestion(Question q) {
+        displayMessage(q.getQuestion());
+        List<String> alternatives = q.getAlternatives();
+        for (int i = 0; i < buttonPanel.getChildren().size(); i++) {
+            Button b = (Button) buttonPanel.getChildren().get(i);
+            b.setText(alternatives.get(i));
+        }
+        setAnswersDisable(false);
+    }
+
+    private Alert getMessageDialog(String title, String header, String content) {
+        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+        dialog.setTitle(title);
+        dialog.setHeaderText(header);
+        dialog.setContentText(content);
+        return dialog;
+    }
+
+    private void processServerMessage(ServerMessage fromServer) {
+        if (fromServer.HEADER == ServerMessage.Headers.WELCOME) {
+            if (fromServer.MESSAGE.contains("1")) {
+                playerOne.setText(fromServer.MESSAGE);
+                playerTwo.setText("Player 2");
+            } else {
+                playerTwo.setText(fromServer.MESSAGE);
+                playerOne.setText("Player 1");
+            }
+        } else if (fromServer.HEADER == ServerMessage.Headers.WAIT) {
+            setAnswersDisable(true);
+            setCategoryDisable(true);
+            displayMessage(fromServer.MESSAGE);
+        } else if (fromServer.HEADER == ServerMessage.Headers.CHOOSE_CATEGORY) {
+            String[] categories = fromServer.MESSAGE.split(";");
+            categoryChooser.getItems().clear();
+            for (String s : categories) {
+                categoryChooser.getItems().add(s);
+            }
+            setCategoryDisable(false);
+            displayMessage("Choose category");
+        } else if (fromServer.HEADER == ServerMessage.Headers.YOU_WIN) {
+            getMessageDialog(null, "You win!", "Congratulations!").showAndWait();
+        } else if (fromServer.HEADER == ServerMessage.Headers.YOU_LOSE) {
+            getMessageDialog(null, "You lose!", "Too bad!").showAndWait();
+        } else if (fromServer.HEADER == ServerMessage.Headers.YOU_TIED) {
+            getMessageDialog(null, "You tied!", "How unexpected!").showAndWait();
+        } else {
+            displayMessage(fromServer.MESSAGE);
+        }
+    }
+
+    void setAnswersDisable(boolean b) {
+        buttonPanel.setDisable(b);
+    }
+
+    void setCategoryDisable(boolean b) {
+        categoryPanel.setDisable(b);
     }
 }
